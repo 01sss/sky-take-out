dfb7f45f5645d6cc33f15f00adaa786767f02c6d
Tan <tan2868924851@gmail.com>
2024-08-21 22:14:34 +0800

    员工分页查询业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,16 +1,20 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -32,7 +37,7 @@ public class EmployeeServiceImpl implements EmployeeService {
      * @return
      */
     public Employee login(EmployeeLoginDTO employeeLoginDTO) {
-          String username = employeeLoginDTO.getUsername();
+        String username = employeeLoginDTO.getUsername();
         String password = employeeLoginDTO.getPassword();
 
         //1、根据用户名查询数据库中的数据
@@ -63,7 +68,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
 
-    /*
+    /**
     * 新增员工
     *   对员工属性进行设置
     */
@@ -89,6 +94,28 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         // 将员工属性封装好之后，调用持久层将上面的数据插入
         employeeMapper.insert(employee);
+    }
+
+
+    /**
+     * 分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+
+    public PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO) {
+        // 底层原理：使用sql语句将数据库中对应的数据进行分页(例：select * from employee limit 0,10)
+        // 也就是说需要动态的获得当前页码和每页的数据量
+
+        // 开始分页查询   PageHelper的作用是在底层使用sql语句进行查询的时候在末尾动态添加上limit #{page}, #{pageSzie}语句
+        PageHelper.startPage(employeePageQueryDTO.getPage(), employeePageQueryDTO.getPageSize());
+
+        Page<Employee> page = employeeMapper.pageQuery(employeePageQueryDTO);
+
+        long total = page.getTotal();
+        List<Employee> records = page.getResult();
+
+        return new PageResult(total, records);
     }
 
 }
